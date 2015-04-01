92103933ce0019f729780572a79a0fc95c61e9cf
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-04-01 13:45:28 -0400

    problem 3 of javascrpit 101

---
+++ string.js
@@ -0,0 +1,17 @@
+/*
+ * Ryan Postma
+ * CMP344
+ * Exam 2
+ * Javascript 101 problem 3
+ */
+
+//store the string value
+write("enter a string value: ");
+var string = readline();
+
+//enter a number
+write("enter a number value: ");
+var number = readline();
+
+//print out what they enterd
+print("You entered " + string + " for your string input, and " + number + " for your number.");
